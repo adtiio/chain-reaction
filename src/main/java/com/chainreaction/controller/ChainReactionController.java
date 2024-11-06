5c01a253f312a74d1b1bcfc86981e98be2cc24c7
adtiio <145439753+adtiio@users.noreply.github.com>
2024-11-06 15:55:27 +0530

    Update ChainReactionController.java

--- src/main/java/com/chainreaction/controller/ChainReactionController.java
+++ src/main/java/com/chainreaction/controller/ChainReactionController.java
@@ -1,13 +1,28 @@
 package com.chainreaction.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import java.util.*;
 
 @Controller
 public class ChainReactionController {
 
-    @GetMapping("/home")
-    public String home(){
-        return "index";
-    }
+  @GetMapping("/home")
+  public String home() {
+    return "index";
+  }
+
+  @GetMapping("/new-game")
+  public ModelAndView newGame() {
+    String sessionId = Integer.toString((int)(Math.random()*1e9)) ;
+    return new ModelAndView("redirect:/game").addObject("sessionId", sessionId);
+  }
+
+  @GetMapping("/game")
+  public ModelAndView game(@RequestParam(value = "sessionId", required = false) String sessionId) {
+    ModelAndView modelAndView = new ModelAndView("game");
+    modelAndView.addObject("sessionId", sessionId);
+    return modelAndView;
+  }
 }
